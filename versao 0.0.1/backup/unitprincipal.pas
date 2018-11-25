eec266bcbde4916c2e5254a21420a42df36b705f
CARLOS DANIEL <carlosfaita99@gmail.com>
2018-11-25 03:14:21 -0200

    protótipo: 0.0.1

---
+++ versao 0.0.1/backup/unitprincipal.pas
@@ -0,0 +1,41 @@
+unit unitprincipal;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  ExtCtrls;
+
+type
+
+  { TformLogin }
+
+  TformLogin = class(TForm)
+    ButtonEntrar: TButton;
+    EditSenha: TEdit;
+    EditUsuario: TEdit;
+    GroupBoxSenha: TGroupBox;
+    GroupBoxUsuario: TGroupBox;
+    GroupBoxLogin: TGroupBox;
+    Label1: TLabel;
+    LabelSuporteTecnico: TLabel;
+    LabelResetarSenha: TLabel;
+    LabelTitulo: TLabel;
+    PanelFoter: TPanel;
+  private
+
+  public
+
+  end;
+
+var
+  formLogin: TformLogin;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
---
+++ versao 0.0.1/fprincipal.lpr
@@ -0,0 +1,21 @@
+program fprincipal;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, unitprincipal
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+  RequireDerivedFormResource:=True;
+  Application.Initialize;
+  Application.CreateForm(TformLogin, formLogin);
+  Application.Run;
+end.
+
